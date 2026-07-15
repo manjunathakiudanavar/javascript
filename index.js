7a5c97171b72a572547b705d35b3b17225749166
manjunath k <manjunath.kiudanavar@gmail.com>
2026-07-16 01:37:07 +0530

    javasccript brush up

--- index.js
+++ index.js
@@ -1,17 +1,74 @@
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
 
-function add(a,b){
-    console.log(a+b);
+// function add(a,b){
+//     console.log(a+b);
     
 
-}
-bdd(5,5)
+// }
+// add(5,5)
+//VARIABLES
+// var name= "manjunath"; //here in var keyword we can reassign the variable value and this is not the good pactise instead use let keyword
+// var name="Alex";
+// console.log(name);
+// let name = "manjunath";
+// //let name = "alex"; //this will throw referance error saying name has been already declared instead that take variable name and reassign like this 
+// name = "alex" //after reassigning the output will be the reassigned name  
+// console.log(name);
+// const age = 26;
+// age = 27;
+
+
+//  primitive data types
+//number data type
+// let num = 50;
+// console.log(typeof num );
+
+//string data type
+// let a = "hello wrold ";
+// console.log(typeof a);
+
+//boolean 
+// let bool = true;
+// console.log(typeof bool);
+
+//null
+// let b= null;
+// console.log(typeof b);
+
+// Undefined
+// let c;
+// console.log(typeof c);
+
+
+//Type conversions
+
+// result = "30"+40;
+// result = String(result);
+// console.log(result);
+// console.log(typeof result);
+
+
+// Operators
+// let x=5
+// console.log(x++);
+// console.log(x);
+
+let x=5;
+console.log(++x);
+console.log(x);
+
+
+
+
+
+
+
 
 
