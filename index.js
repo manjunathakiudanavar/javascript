3db9fa4240881cff6be1d2e6b0ffe0dee1615fa8
manjunath k <manjunath.kiudanavar@gmail.com>
2026-07-16 16:28:43 +0530

    javascript brush up

--- index.js
+++ index.js
@@ -60,9 +60,20 @@
 // console.log(x++);
 // console.log(x);
 
-let x=5;
-console.log(++x);
-console.log(x);
+// let x=5;
+// console.log(++x);
+// console.log(x);
+
+//Logical operators
+// OR ||
+// console.log(3>4 || 4>3); >> if anyone is true the out put will be true >>
+
+// AND
+
+// console.log(2>3 && 2>1); >> if anyone is false then the out will be false 
+
+// NOT
+// console.log(!true); visa versa if true out put will be false 
 
 
 
